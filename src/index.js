ade5bdaa5ca3a17c4baafe4fcc0956d7ab4c5084
Manuela França <manuelaalves26@gmail.com>
2022-10-04 19:05:54 -0300

    Requisito 2.

--- src/index.js
+++ src/index.js
@@ -14,11 +14,20 @@ const pathTalker = path.resolve(__dirname, '..', 'src', 'talker.json');
 
 app.get('/talker', async (_req, res) => {
   const speakingPeople = JSON.parse(await fs.readFile(pathTalker, 'utf8'));
-  res.status(200).json(speakingPeople);
+  res.status(HTTP_OK_STATUS).json(speakingPeople);
   if (!speakingPeople) {
-    return res.status(200).send([]);
+    return res.status(HTTP_OK_STATUS).send([]);
   }
 });
+
+app.get('/talker/:id', async (req, res) => {
+    const speakingPeople = JSON.parse(await fs.readFile(pathTalker, 'utf8'));
+    const index = speakingPeople.find(({ id }) => id === Number(req.params.id));
+    if (index) {
+      return res.status(HTTP_OK_STATUS).json(index);
+    }
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  });
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
