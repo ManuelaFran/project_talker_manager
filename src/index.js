0a6934ff8d90dd4b5b1d1108b914462db67d08f8
Manuela França <manuelaalves26@gmail.com>
2022-10-04 16:02:06 -0300

    Requisito 1.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.json());
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+const pathTalker = path.resolve(__dirname, '..', 'src', 'talker.json');
+
+app.get('/talker', async (_req, res) => {
+  const speakingPeople = JSON.parse(await fs.readFile(pathTalker, 'utf8'));
+  res.status(200).json(speakingPeople);
+  if (!speakingPeople) {
+    return res.status(200).send([]);
+  }
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -13,5 +26,5 @@ app.get('/', (_request, response) => {
 });
 
 app.listen(PORT, () => {
-  console.log('Online!');
+  console.log('Online');
 });
