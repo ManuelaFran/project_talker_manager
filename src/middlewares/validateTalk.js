8bda9df0dfa326f994f8b6149590b2b0f2facfb1
Manuela França <manuelaalves26@gmail.com>
2022-10-06 10:07:00 -0300

    Requisitos do 6 a 8.

--- src/middlewares/validateTalk.js
+++ src/middlewares/validateTalk.js
@@ -1,17 +1,3 @@
-// const validateTalk = (talkValue, res, value) => {
-//   if (!talkValue) {
-//     return res.status(400).json({ message: `O campo ${value} é obrigatório` });
-//   }
-// };
-
-// module.exports = (req, res, next) => {
-//   const { talk } = req.body;
-
-//  return validateTalk(talk, res, '"talk"')
-//  || validateTalk(talk.watchedAt, res, '"watchedAt"')
-//  || next();
-// };
-
 module.exports = (req, res, next) => {
     const { talk } = req.body;
     if (!talk) {
